599f29212b5d46a03df91f50dec766c0e75234a0
Ethosania <ethosania@gmail.com>
2022-10-27 10:09:28 +1100

    #1312 Added unit tests

--- row-data-gateway/src/test/java/com/iluwatar/rowDataGateway/rowDataGatewayTest.java
+++ row-data-gateway/src/test/java/com/iluwatar/rowDataGateway/rowDataGatewayTest.java
@@ -1,16 +1,59 @@
 package com.iluwatar.rowDataGateway;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class rowDataGatewayTest {
-    //setup
+    @Test
+    void initialiseTest() {
+        assertDoesNotThrow(App::initialize);
+        App.clearAll();
+    }
 
-    //delete
+    @Test
+    void clearAllTest() {
+        assertDoesNotThrow(App::clearAll);
+    }
 
-    //insert
+    @Test
+    void insertTest() {
+        rowData r1 = new rowData(1,"s",1);
+        App.initialize();
+        rowDataGateway rDG1 = new rowDataGateway(r1,"jdbc:sqlite:test.db");
+        assertDoesNotThrow(rDG1::insert);
+        App.clearAll();
+    }
 
-    //update
+    @Test
+    void updateTest() {
+        rowData r1 = new rowData(1,"s",1);
+        rowData r2 = new rowData(1,"t",2);
+        App.initialize();
+        rowDataGateway rDG1 = new rowDataGateway(r1,"jdbc:sqlite:test.db");
+        rDG1.insert();
+        rDG1.setRowData(r2);
+        assertDoesNotThrow(rDG1::update);
+        App.clearAll();
+    }
 
-    //delete
+    @Test
+    void deleteTest() {
+        rowData r1 = new rowData(1,"s",1);
+        App.initialize();
+        rowDataGateway rDG1 = new rowDataGateway(r1,"jdbc:sqlite:test.db");
+        rDG1.insert();
+        assertDoesNotThrow(rDG1::delete);
+        App.clearAll();
+    }
+
+    @Test
+    void setRowDataTest() {
+        rowData r1 = new rowData(1,"s",1);
+        rowData r2 = new rowData(1,"t",2);
+        rowDataGateway rDG1 = new rowDataGateway(r1,"jdbc:sqlite:test.db");
+        rDG1.setRowData(r2);
+        assertEquals(r2,rDG1.getRowData());
+    }
 }
--- row-data-gateway/src/test/java/com/iluwatar/rowDataGateway/rowDataTest.java
+++ row-data-gateway/src/test/java/com/iluwatar/rowDataGateway/rowDataTest.java
@@ -1,13 +1,52 @@
 package com.iluwatar.rowDataGateway;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class rowDataTest {
-    //equals check
-    //not equals check
+    @Test
+    void obviousEquals() {
+        rowData r1 = new rowData(1, "Joe", 20);
+        rowData r2 = new rowData(1, "Joe", 20);
+        assertEquals(r1.getID(),r2.getID());
+        assertEquals(r1.getName(),r2.getName());
+        assertEquals(r1.getValue(),r2.getValue());
+    }
 
-    //not equals check for rowDatas element by element?
+    @Test
+    void idNotEquals() {
+        assertNotEquals(new rowData(1, "Joe", 20), (new rowData(2, "Joe", 20)));
+    }
 
-    //test setting elements
+    @Test
+    void stringNotEquals() {
+        assertNotEquals(new rowData(1, "Joe", 20), (new rowData(1, null, 20)));
+    }
+
+    @Test
+    void valueNotEquals() {
+        assertNotEquals(new rowData(1, "Joe", 20), (new rowData(1, "Joe", 21)));
+    }
+
+    @Test
+    void setNameTest() {
+        rowData r1 = new rowData(1, "Joe", 20);
+        r1.setName("Joseph");
+        assertEquals("Joseph", r1.getName());
+    }
+
+    @Test
+    void setIDTest() {
+        rowData r1 = new rowData(1, "Joe", 20);
+        r1.setID(2);
+        assertEquals(2, r1.getID());
+    }
+
+    @Test
+    void setValueTest() {
+        rowData r1 = new rowData(1, "Joe", 20);
+        r1.setValue(21);
+        assertEquals(21, r1.getValue());
+    }
 }
